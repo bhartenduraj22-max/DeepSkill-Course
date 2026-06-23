6bd810da342a5025d3438ace2f20c6434da61b0e
bhartenduraj22-max <bhartenduraj22@gmail.com>
2026-06-23 23:10:40 +0530

    Update SingletonPattern.java

--- Week 1/Implementing Singleton Pattern/SingletonPattern.java
+++ Week 1/Implementing Singleton Pattern/SingletonPattern.java
@@ -1,1 +1,17 @@
+public class SingletonPattern {
 
+    public static void main(String[] args) {
+
+        Logger firstLogger = Logger.getInstance();
+        Logger secondLogger = Logger.getInstance();
+
+        firstLogger.log("Application started");
+        secondLogger.log("User logged in");
+
+        if (firstLogger == secondLogger) {
+            System.out.println("Only one Logger instance exists.");
+        } else {
+            System.out.println("Multiple Logger instances exist.");
+        }
+    }
+}
