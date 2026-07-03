5ade1b7f88d0a34ab9fad944b404b59def3ac20f
bhartenduraj22-max <bhartenduraj22@gmail.com>
2026-07-03 09:55:03 +0530

    Update ProcessMonthlyInterest.sql

--- Week 1/Stored Procedures/ProcessMonthlyInterest.sql
+++ Week 1/Stored Procedures/ProcessMonthlyInterest.sql
@@ -1,1 +1,14 @@
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+AS
+BEGIN
 
+    UPDATE Accounts
+    SET Balance = Balance + (Balance * 0.01)
+    WHERE AccountType = 'Savings';
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Monthly interest added successfully.');
+
+END;
+/
