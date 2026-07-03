c32034264af884ec19e49f78cd34d3b446e4e97c
bhartenduraj22-max <bhartenduraj22@gmail.com>
2026-07-03 08:48:24 +0530

    Update ForecastDemo.java

--- Week 1/Financial Forecasting/ForecastDemo.java
+++ Week 1/Financial Forecasting/ForecastDemo.java
@@ -1,1 +1,16 @@
+public class ForecastDemo {
 
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10;
+        int years = 5;
+
+        double futureValue = FinancialForecast.predictValue(currentValue, growthRate, years);
+
+        System.out.println("Current Value : " + currentValue);
+        System.out.println("Growth Rate   : " + (growthRate * 100) + "%");
+        System.out.println("Years         : " + years);
+        System.out.printf("Future Value  : %.2f", futureValue);
+    }
+}
