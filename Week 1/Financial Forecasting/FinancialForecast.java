9454706eaa0bce4948e58ab02668e0d2ee3080bc
bhartenduraj22-max <bhartenduraj22@gmail.com>
2026-07-03 08:47:59 +0530

    Update FinancialForecast.java

--- Week 1/Financial Forecasting/FinancialForecast.java
+++ Week 1/Financial Forecasting/FinancialForecast.java
@@ -1,1 +1,11 @@
+public class FinancialForecast {
 
+    public static double predictValue(double currentValue, double growthRate, int years) {
+
+        if (years == 0) {
+            return currentValue;
+        }
+
+        return predictValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+}
