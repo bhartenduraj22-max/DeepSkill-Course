6830a2d557aa292895bbfec558ce40da3208b5da
bhartenduraj22-max <bhartenduraj22@gmail.com>
2026-07-03 13:58:20 +0530

    Create BookService.java

---
+++ Week 2/Implementing Dependency Injection/BookService.java
@@ -0,0 +1,13 @@
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void showBook() {
+        bookRepository.displayBook();
+    }
+}
