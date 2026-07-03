74ac1cde824a139a0d4e888587deeed142c1af04
bhartenduraj22-max <bhartenduraj22@gmail.com>
2026-07-03 09:27:58 +0530

    Update Scenario3.sql

--- Week 1/Control Structures/Scenario3.sql
+++ Week 1/Control Structures/Scenario3.sql
@@ -1,1 +1,22 @@
+BEGIN
 
+    FOR customer IN (
+        SELECT CustomerID, Age, LoanInterestRate
+        FROM Customers
+    )
+    LOOP
+
+        IF customer.Age > 60 THEN
+
+            UPDATE Customers
+            SET LoanInterestRate = LoanInterestRate - 1
+            WHERE CustomerID = customer.CustomerID;
+
+        END IF;
+
+    END LOOP;
+
+    COMMIT;
+
+END;
+/
