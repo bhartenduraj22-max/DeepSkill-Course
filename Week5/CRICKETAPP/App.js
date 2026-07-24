9854ed7d6f22c6122d4d6c6648d8b4fb14706318
Bhartendu Raj Akarsh <bhartenduraj22@gmail.com>
2026-07-24 20:13:31 +0530

    Update App.js

--- Week5/CRICKETAPP/App.js
+++ Week5/CRICKETAPP/App.js
@@ -1,1 +1,30 @@
+import ListofPlayers from "./ListofPlayers";
+import {
+  OddPlayers,
+  EvenPlayers,
+  IndianPlayers
+} from "./IndianPlayers";
 
+function App() {
+  return (
+    <div>
+      <ListofPlayers />
+
+      <h1>Indian Team</h1>
+
+      <h2>Odd Players</h2>
+      {OddPlayers(IndianPlayers)}
+
+      <h2>Even Players</h2>
+      {EvenPlayers(IndianPlayers)}
+
+      <h2>List of Indian Players Merged:</h2>
+
+      {IndianPlayers.map((player, index) => (
+        <div key={index}>Mr. {player}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
