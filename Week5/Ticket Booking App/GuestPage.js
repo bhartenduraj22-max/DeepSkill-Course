e35916683707a00a80203974e5b3ae0f92679e5d
Bhartendu Raj Akarsh <bhartenduraj22@gmail.com>
2026-07-24 21:04:40 +0530

    Update GuestPage.js

--- Week5/Ticket Booking App/GuestPage.js
+++ Week5/Ticket Booking App/GuestPage.js
@@ -1,1 +1,15 @@
+function GuestPage() {
+  return (
+    <div>
+      <h1>Please sign up.</h1>
 
+      <h2>Flight Details</h2>
+      <p>Airline: Air India</p>
+      <p>From: Delhi</p>
+      <p>To: Mumbai</p>
+      <p>Price: Rs. 5000</p>
+    </div>
+  );
+}
+
+export default GuestPage;
