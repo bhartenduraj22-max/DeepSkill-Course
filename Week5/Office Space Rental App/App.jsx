c5aef0b15257b7fc7dd3a4cbaca8f7cb62b88328
Bhartendu Raj Akarsh <bhartenduraj22@gmail.com>
2026-07-24 20:25:04 +0530

    Update App.jsx

--- Week5/Office Space Rental App/App.jsx
+++ Week5/Office Space Rental App/App.jsx
@@ -1,1 +1,61 @@
+function App() {
+  const element = "Office Space";
 
+  const image =
+    "https://images.unsplash.com/photo-1497366754035-f200968a6e72?w=500";
+
+  const offices = [
+    {
+      Name: "DBS",
+      Rent: 50000,
+      Address: "Chennai",
+    },
+    {
+      Name: "WeWork",
+      Rent: 65000,
+      Address: "Bangalore",
+    },
+    {
+      Name: "Regus",
+      Rent: 55000,
+      Address: "Hyderabad",
+    },
+  ];
+
+  return (
+    <div style={{ margin: "40px" }}>
+      <h1>{element}, at Affordable Range</h1>
+
+      {offices.map((item, index) => {
+        let rentColor;
+
+        if (item.Rent <= 60000) {
+          rentColor = "red";
+        } else {
+          rentColor = "green";
+        }
+
+        return (
+          <div key={index} style={{ marginBottom: "40px" }}>
+            <img
+              src={image}
+              width="250"
+              height="200"
+              alt="Office Space"
+            />
+
+            <h1>Name: {item.Name}</h1>
+
+            <h3 style={{ color: rentColor }}>
+              Rent: Rs. {item.Rent}
+            </h3>
+
+            <h3>Address: {item.Address}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
