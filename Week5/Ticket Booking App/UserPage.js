32266991ef1952a10961c3534b2a4723d977c9a4
Bhartendu Raj Akarsh <bhartenduraj22@gmail.com>
2026-07-24 21:05:58 +0530

    Update UserPage.js

--- Week5/Ticket Booking App/UserPage.js
+++ Week5/Ticket Booking App/UserPage.js
@@ -1,1 +1,18 @@
+function UserPage() {
+  return (
+    <div>
+      <h1>Welcome back</h1>
 
+      <h2>Book Your Ticket</h2>
+
+      <p>Airline: Air India</p>
+      <p>From: Delhi</p>
+      <p>To: Mumbai</p>
+      <p>Price: Rs. 5000</p>
+
+      <button>Book Ticket</button>
+    </div>
+  );
+}
+
+export default UserPage;
